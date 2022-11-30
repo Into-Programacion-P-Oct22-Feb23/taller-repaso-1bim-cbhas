a56d4ecad50f8a3acb64e6091e46f4003ea85789
Sebastián Calderón <calderonsebastian211@gmail.com>
2022-11-30 10:15:17 -0500

    Repaso

---
+++ solucion/repaso01/src/repaso01/Repaso01.java
@@ -0,0 +1,89 @@
+package repaso01;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Repaso01 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        String nombreEmpleado;
+        String mensaje = "";
+        String opcion = "s";
+        String ciudad;
+        int cedula;
+        int horasMatutinas;
+        int horasNocturnas;
+        double valorMatutino = 10;
+        double valorNocturno = 15;
+        double adicionalNocturno = 0;
+        double valorSocial = 18;
+        double totalMatutino;
+        double totalNocturno;
+        double subtotal;
+        double sueldoFinal;
+        int contador = 0;
+        boolean bandera = true;
+
+        while (bandera) {
+            System.out.println("Ingrese el nombre del empleado: ");
+            nombreEmpleado = entrada.nextLine();
+
+            System.out.println("Ingrese la ciudad de la empresa: ");
+            ciudad = entrada.nextLine();
+
+            System.out.println("Ingrese la cédula del empleado: ");
+            cedula = entrada.nextInt();
+
+            System.out.println("Ingrese las horas matutinas trabajadas del "
+                    + "empleado: ");
+            horasMatutinas = entrada.nextInt();
+
+            System.out.println("Ingrese las horas nocturnas trabajadas del "
+                    + "empleado: ");
+            horasNocturnas = entrada.nextInt();
+
+            totalMatutino = horasMatutinas * valorMatutino;
+
+            if (horasNocturnas >= 10) {
+                adicionalNocturno = (valorNocturno * 10) / 100;
+            }
+
+            valorNocturno = valorNocturno + adicionalNocturno;
+            totalNocturno = valorNocturno * horasNocturnas;
+            subtotal =  totalMatutino + totalNocturno;
+            
+            valorSocial = (subtotal * valorSocial) / 100;
+            sueldoFinal = subtotal - valorSocial;
+
+            mensaje = String.format("Rol del trabajador de nombre %s y cédula %d."
+                    + "\n\tHoras matutina trabajadas: %d.\n\tHoras nocturnas "
+                    + "trabajadas: %d.\n\tSubtotal $%.2f\n\tSeguro Social $%.1f"
+                    + "\n\tTotal a cancelar $%.1f\n\n",
+                    nombreEmpleado,
+                    cedula, horasMatutinas,
+                    horasNocturnas,
+                    subtotal,
+                    valorSocial,
+                    sueldoFinal);
+            
+            entrada.nextLine();
+
+            contador = contador + 1;
+
+            System.out.println("Si ya no quiere seguir ingresando datos, "
+                    + "presione s ");
+            opcion = entrada.nextLine();
+
+            if (opcion.equals("s")) {
+                bandera = false;
+            }
+
+        }
+        System.out.printf("Nómina de trabajadores\n\n%sNúmero de empleados %d",
+                mensaje,
+                contador);
+    }
+}
